93d693ce245442fdf2c0a4bd621edd5ac9e124b5
Ray-Palmer <89266065+IT21227554@users.noreply.github.com>
2022-02-09 06:28:41 +0000

    tute 02

--- Tute02.c
+++ Tute02.c
@@ -20,5 +20,24 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 
 int main() {
   
+  // declaring variable 
+  int distance ;
+  double amount = 0;
+ 
+ // taking input from the user
+  printf("Enter the distance the can has travelled : ");
+  scanf("%d", &distance );
+
+ // checking if the distance is higher than 30KM and calculating amount to be paid
+  if(distance > 30 ){
+    amount = 40* (distance - 30);
+    distance = distance - (distance - 30);
+  }
+
+  amount = amount + (distance * 50);
+
+ // print the amount user has to pay
+  printf("Amount to be paid :- %.2lf" , amount);
+
   return 0;
 }
