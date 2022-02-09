93a6a7cebe796e09c3f4e084e04a8603958417bf
Ray-Palmer <89266065+IT21227554@users.noreply.github.com>
2022-02-09 06:16:30 +0000

    tute 01

--- Tute01.c
+++ Tute01.c
@@ -6,6 +6,26 @@
 
 int main() {
   
+ // declaring variables for loop and take input
+  int i ;
+  float mark , total = 0 , avg ;
+
+ // for loop for input marks of 3 subject 
+  for( i = 0 ; i < 2 ; i++){
+    
+    printf("Enter mark %d : ",i+1);
+    scanf("%f", &mark);
+  
+ // calculating total
+    total += mark;
+  }
+
+ // calculate Average
+  avg = total / 2;
+
+ // print average
+  printf("\nAverage mark of student %.2f\n", avg);
+
   return 0;
 }
 
